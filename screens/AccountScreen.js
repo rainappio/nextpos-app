bb63b4e1207ff8d93df2fa929b4be72d409e4de1
Loxi <dianyuyi@gmail.com>
2021-04-16 10:16:14 +0800

    RA254
    fix split bill: split by head will delete order

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -204,7 +204,7 @@ class AccountScreen extends React.Component {
             </View>
             <View style={[styles.tableCellView, {flex: 3, justifyContent: 'flex-end'}]}>
               <StyledText>
-                2021-03-31
+                2021-04-16
               </StyledText>
             </View>
           </View>
