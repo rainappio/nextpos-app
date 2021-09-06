4bc2211de3006a5d88a6579ade84205913037b89
Loxi <dianyuyi@gmail.com>
2021-09-06 14:16:43 +0800

    Fix notification
    change mode

--- screens/ReservationUpcomingForm.js
+++ screens/ReservationUpcomingForm.js
@@ -255,7 +255,7 @@ class ReservationUpcomingForm extends React.Component {
 
               }
             ).then(() => {
-              if (this.props.client?.clientSettings?.PUSH_NOTIFICATION && !!this.props.client?.clientSettings?.PUSH_NOTIFICATION?.value) {
+              if (this.props.client?.clientSettings?.PUSH_NOTIFICATION !== undefined && this.props.client?.clientSettings?.PUSH_NOTIFICATION?.value) {
                 notificationPush(reservation, t, flag)
               }
               setTimeout(() => {
